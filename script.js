eeb5919ece4a276d6c6148cb3df8902eceb55739
Muhammad Abrar <120363640+muhammadabrar99@users.noreply.github.com>
2024-01-02 21:29:42 +0500

    make changes in js file

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 import { localQuotes } from "./quotes.mjs";
 
-console.log(localQuotes[1]);
+const quoteContainer = document.getElementById("Quote-container");
+const quoteText = document.getElementById("Quote");
+const authorText = document.getElementById("Author-text");
+const twitterBtn = document.getElementById("Twitter");
+const newQuoteBtn = document.getElementById("New-Quote");
+
+function newQuote() {
+  // Pick new Quote from Quotes.mjs
+  const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+  console.log(quote);
+}
+
+// Output
+newQuote();
